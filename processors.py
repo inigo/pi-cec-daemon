2835601e206a50d7f124420433e21ed3a029dd35
Inigo Surguy <inigo.surguy@67bricks.com>
2026-01-03 13:58:04 +0000

    Add setup

--- processors.py
+++ processors.py
@@ -1,8 +1,9 @@
 import logging
 import time
+import threading
 
-from cec_comms import CECCommand
-from cec_delegate import with_timeout
+from cec_comms import CECCommand, CECComms
+from cec_delegate import with_timeout, CECEventBus
 from devices import PowerStatus, CECOpcode, UserControlCode
 
 
@@ -203,3 +204,73 @@ def SwitchStatusProcessor(eventbus, addresses):
 
         # Wait for next event
         cmd = yield []
+
+
+class ProcessorManager:
+    """
+    Top-level manager for CEC processors.
+
+    Sets up the event bus, addresses, and manages processor lifecycle.
+    """
+
+    def __init__(self, comms: CECComms):
+        """
+        Initialize the processor manager.
+
+        Args:
+            comms: CECComms instance (RealCECComms or MockCECComms)
+        """
+        self.logger = logging.getLogger('ProcessorManager')
+        self.comms = comms
+        self.eventbus = CECEventBus(comms)
+        self.addresses = Addresses()
+        self._timer = None
+        self._running = False
+
+    def start(self):
+        """Initialize the event bus and start all processors"""
+        self.logger.info("Starting processor manager")
+
+        # Initialize the event bus
+        if not self.eventbus.init():
+            self.logger.error("Failed to initialize event bus")
+            return False
+
+        # Add SwitchStatusProcessor (runs forever)
+        self.logger.info("Adding SwitchStatusProcessor")
+        self.eventbus.add_processor(SwitchStatusProcessor(self.eventbus, self.addresses))
+
+        # Start periodic SoundbarOnWithTvProcessor spawning
+        self._running = True
+        self._spawn_soundbar_on_with_tv_processor()
+
+        self.logger.info("Processor manager started")
+        return True
+
+    def _spawn_soundbar_on_with_tv_processor(self):
+        """Periodically spawn SoundbarOnWithTvProcessor every 500ms"""
+        if not self._running:
+            return
+
+        self.logger.debug("Spawning SoundbarOnWithTvProcessor")
+        self.eventbus.add_processor(SoundbarOnWithTvProcessor(self.eventbus, self.addresses))
+
+        # Schedule next spawn in 500ms
+        self._timer = threading.Timer(0.5, self._spawn_soundbar_on_with_tv_processor)
+        self._timer.daemon = True
+        self._timer.start()
+
+    def stop(self):
+        """Stop the processor manager and clean up resources"""
+        self.logger.info("Stopping processor manager")
+        self._running = False
+
+        # Cancel the timer if it's running
+        if self._timer:
+            self._timer.cancel()
+            self._timer = None
+
+        # Close the event bus
+        self.eventbus.close()
+
+        self.logger.info("Processor manager stopped")
